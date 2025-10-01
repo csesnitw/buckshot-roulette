3f5ce00be60526d0fac0b36b27e44cac8b46fe6a
JeffreyJ2006 <jeffreyseelan@gmail.com>
2025-10-01 18:06:45 +0530

    added the rules for matches and rounds

--- gameManager.gd
+++ gameManager.gd
@@ -1,8 +1,8 @@
 extends Node
 
 class GameState:
-	var alivePlayers: Array[Player]
-	var upgradesOnTable: Array[Upgrade]
+	var alivePlayers
+	var upgradesOnTable
 
 	func _init(_alivePlayers: Array, _upgradesOnTable: Array):
 		alivePlayers = _alivePlayers
@@ -10,64 +10,99 @@ class GameState:
 		
 # TODO: make the Player class please and Upgrade too
 var gameState: GameState = GameState.new([], [])
-var players: Array[Player] = []
+var players = []
 var currPlayerTurnIndex: int = 0 
 var shotgunShells: Array[int] = [] # 0 for blank, 1 for live
-var tableUpgrades: Array[Upgrades] = []
-var roundIndex: int = 0;
-var shotgunShellCount: int = 8; # some logic based on round index
+var tableUpgrades = []
+var roundIndex: int = 0
+var shotgunShellCount: int = 8 # some logic based on round index
 
-# Game Logic functions
+# ======================================================
+# GAME LOGIC FUNCTIONS
+# ======================================================
+
 func initMatch() -> void:
+	# Start a new match (first round)
 	roundIndex = 0
-	shotgunShellCount = 8;
+	shotgunShellCount = 8
+	initRound()
 
 func initRound() -> void:
+	# Prepare new round
 	currPlayerTurnIndex = 0
+	roundIndex += 1
+	# Reset shotgun shells for this round
+	generateRandomBullets()
+	# Reset upgrades on table if needed
+	tableUpgrades.clear()
+	# Ensure all alive players are still in GameState
+	gameState.alivePlayers = players.filter(func(p): return p.isAlive)
 
 func endTurn() -> void:
+	# Move to next alive player
 	currPlayerTurnIndex += 1
+	if currPlayerTurnIndex >= gameState.alivePlayers.size():
+		currPlayerTurnIndex = 0
+	# If only one player alive → match ends
+	if checkWin():
+		endGame()
 
 func checkWin() -> bool:
-	return alivePlayers.size() == 1;
+	return gameState.alivePlayers.size() == 1
 
 func generateRandomBullets():
 	shotgunShells.clear()
 	for i in range(shotgunShellCount):
 		var shell = randi() % 2  
-		shotgunShells[i] = shell
+		shotgunShells.append(shell)
 
-# Below are all functions that are player facing, call these when designing players for player devs
+# ======================================================
+# PLAYER-FACING FUNCTIONS
+# ======================================================
+
 func endGame() -> void:
-	return
+	print("Game Over! Winner: ", gameState.alivePlayers[0].name if gameState.alivePlayers.size() == 1 else "None")
+
 func getGameState() -> GameState:
 	return gameState
 
-func shootPlayer(callerPlayerRef: Player, targetPlayerRef: Player) -> void:
-	# logic for shooting
+func shootPlayer(callerPlayerRef, targetPlayerRef) -> void:
+	# Take the next shell from the shotgun
+	if shotgunShells.size() == 0:
+		# If no shells left, start a new round
+		initRound()
+		return
+	
+	var shell = shotgunShells.pop_front()
+	
+	if shell == 1:
+		# Live round, target takes damage
+		targetPlayerRef.takeDamage(1)
+		print("%s shot %s with a live round!" % [callerPlayerRef.name, targetPlayerRef.name])
+		if not targetPlayerRef.isAlive:
+			gameState.alivePlayers.erase(targetPlayerRef)
+	else:
+		# Blank round
+		print("%s shot %s with a blank round." % [callerPlayerRef.name, targetPlayerRef.name])
+	
+	# Check if round ended
 	if checkWin():
 		endGame()
-	endTurn()
+	else:
+		endTurn()
 
-# call this when you want to pick an upgrade off of the upgrade table
-# returns true if succesffuly picked up
-# otherwise returns false
-func pickUpUpgrade(callerPlayerRef: Player, upgradeRef: Upgrade) -> boolean:
-	# im guessing i need to include logic in the scene to actually add and remove upgrades from the table visually
+func pickUpUpgrade(callerPlayerRef, upgradeRef) -> bool:
 	var gotUpgrade: bool = false
 	var upIndex: int = -1
-	for i in tableUpgrades.size():
+	for i in range(tableUpgrades.size()):
 		if upgradeRef == tableUpgrades[i]:
 			gotUpgrade = true
 			upIndex = i
 	if gotUpgrade:
 		callerPlayerRef.addInventory(upgradeRef)
-		tableUpgrades.pop_at(i)
-	else:
-		return false
-	return true
+		tableUpgrades.pop_at(upIndex)
+		return true
+	return false
 
-func useUpgrade(upgradeRef: Upgrade, callerPlayerRef: Player, targetPlayerRef: Player) -> void:
+func useUpgrade(upgradeRef, callerPlayerRef, targetPlayerRef) -> void:
 	
-
-# Upgrade devs fill out the space below with your upgrade logics
