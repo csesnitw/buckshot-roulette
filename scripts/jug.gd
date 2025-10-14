93d59854cdfef60af2153444bb4dac4b2f614fdf
Samyak Jain <sj24csb0b64@student.nitw.ac.in>
2025-10-14 17:22:58 +0530

    added table and health juice jug

---
+++ scripts/jug.gd
@@ -0,0 +1,48 @@
+extends Node3D
+class_name JuiceJug  # optional, gives a type name for casting
+
+var layers: int
+var juice_layers = []
+var top_layer: int
+
+func create(n):
+	layers = n
+	var juice_invis = $juice_invis
+	var colors
+	if layers == 2:
+		colors = [
+			Color(1,0,0),
+			Color(0,1,0)
+		]
+	elif layers == 3:
+		colors = [
+			Color(1,0,0),
+			Color(1,1,0),
+			Color(0,1,0)
+		]
+	top_layer = layers-1
+	var x = float(1)/layers
+	var full_height = juice_invis.get_aabb().size.y
+	var layer_height = full_height/layers
+	for i in range(layers):
+		var juice_layer = juice_invis.duplicate()
+		var mat = StandardMaterial3D.new()
+		mat.albedo_color = colors[i]
+		juice_layer.material_override = mat
+		juice_layer.scale = Vector3(1.3 + 0.01*i, 1.3*x, 1.3 + 0.01*i)
+		juice_layer.position.y += -full_height/2 + layer_height*i +  layer_height/2
+		juice_layer.visible = true
+		add_child(juice_layer)
+		juice_layers.append(juice_layer)
+
+func drink():
+	juice_layers[top_layer].visible = false
+	top_layer-=1
+	if top_layer < 0:
+		top_layer = 0
+
+func refill():
+	top_layer += 1
+	if top_layer > layers-1:
+		top_layer = layers-1
+	juice_layers[top_layer].visible = true
