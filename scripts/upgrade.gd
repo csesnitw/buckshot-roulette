334ef2bc0e6b12e8cab44049e851b3207c2a480c
Samyak Jain <sj24csb0b64@student.nitw.ac.in>
2025-10-15 18:41:23 +0530

    fix upgrade grid rendering

--- scripts/upgrade.gd
+++ scripts/upgrade.gd
@@ -3,6 +3,8 @@ class_name Upgrade extends Node3D
 enum UpgradeType {cigarette, beer, magGlass, handcuff, expiredMed, inverter, 
 				  burnerPhone, handSaw, adrenaline, disableUpgrade, unoRev, wildCard}
 
+const UPGRADEMESHMAP = {"cigarette": "juice_carton", "beer": "Cup Tea", "magGlass": "Magnifying Glass"}
+
 var upgrade_type: UpgradeType
 var pos: Vector3 # ts is for players to get pos of
 # ts wasn't working when isntantiating so i removed _init
