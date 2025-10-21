9d7140ffba92b7424b443d1c691dd7e6ea17dfe1
ds-bhatia <dhruvsbhatia@gmail.com>
2025-10-21 21:01:05 +0530

    Updated culling to fix wall render issue

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -40,7 +40,7 @@ func initMatch() -> void:
 	# one this fumc is called though a continuous match SHOULD work.
 	# as for UI changes and stuff best to have them as side effects of functions here i think.
 	upgradeScene = preload("res://scenes/upgrade.tscn")
-	roundIndex = 1
+	roundIndex = 0
 	shotgunShellCount = 8
 	for sibs in get_parent().get_children():
 		#print(get_parent().get_children())
